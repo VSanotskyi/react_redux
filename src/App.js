1bbe5ff8559b5d4dc7eb1e10f6f49efd56176483
Vsanotskyi <vsanotskyi@gmail.com>
2024-01-10 20:47:49 +0200

    redux_slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {useDispatch, useSelector} from "react-redux";
+import {selectCounter} from "./store/counterSlice/selector";
+import {decrement, increment} from "./store/counterSlice/counterSlice";
+
 const App = () => {
+    const {counter} = useSelector(selectCounter);
+    const dispatch = useDispatch();
+
     return (
-        <div>App</div>
+        <div style={{display: "flex", gap: "10px"}}>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <span>{counter}</span>
+            <button onClick={() => dispatch(decrement())}>-</button>
+        </div>
     );
 };
 
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+import {counterReducer} from "./counterSlice/counterSlice";
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+});
